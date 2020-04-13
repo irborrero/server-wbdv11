435473675f0a1fe60087a58596bc8eadf44ce238
Guillermo del Aguila <gdelagui@purdue.edu>
2020-04-13 23:48:53 +0200

    changed origin to allow connection from react client in heroku

--- src/main/java/com/example/springbootserver/controllers/UserController.java
+++ src/main/java/com/example/springbootserver/controllers/UserController.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = "https://project-react-client-t11.herokuapp.com", allowCredentials = "true")
 public class UserController {
     @Autowired
     UserService service;
