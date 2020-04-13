b298917529a996b5ee689dde2d4f07b8dcbc2853
Guillermo del Aguila <gdelagui@purdue.edu>
2020-04-13 17:08:56 +0200

    added validation and unvalidation to controller

--- src/main/java/com/example/springbootserver/controllers/UserController.java
+++ src/main/java/com/example/springbootserver/controllers/UserController.java
@@ -1,27 +1,28 @@
 package com.example.springbootserver.controllers;
 
+import com.example.springbootserver.models.Course;
 import com.example.springbootserver.models.User;
 import com.example.springbootserver.repositories.UserRepository;
+import com.example.springbootserver.services.CourseService;
+import com.example.springbootserver.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class UserController {
     @Autowired
-    UserRepository repository;
+    UserService service;
     @PostMapping("/register")
     public User register(
             HttpSession session,
             @RequestBody User user) {
         user.setValidated(Boolean.FALSE);
-        User newUser = repository.save(user);
+        User newUser = service.save(user);
         newUser.setPassword("***");
         session.setAttribute("profile", newUser);
         return newUser;
@@ -42,14 +43,25 @@ public class UserController {
             @RequestBody User user) {
 
         try {
-            User currentUser = repository.findUserByCredentials(user.getName(), user.getPassword());
+            User currentUser = service.findUserByCredentials(user.getName(), user.getPassword());
             currentUser.setPassword("***");
             session.setAttribute("profile", currentUser);
             return currentUser ;
         } catch (java.lang.NullPointerException e){
             return user;
         }
-
-
     }
+    @GetMapping("/users/faculty/validate")
+    public List<User> findUserToValidate() {
+        return service.findUsersToValidate();
+    }
+    @PutMapping("/users/faculty/validate/{userId}")
+    public void updateValidateFaculty(@PathVariable("userId") int userId) {
+        service.updateValidateFaculty(userId);
+    }
+    @PutMapping("/users/faculty/unvalidate/{userId}")
+    public void updateUnvalidateFaculty(@PathVariable("userId") int userId) {
+        service.updateUnvalidateFaculty(userId);
+    }
+
 }
